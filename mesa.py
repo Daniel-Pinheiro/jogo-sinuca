41024b9a9e885616199c292822cb02cb0c15ee95
Lucas Amoêdo <lucas_advc@outlook.com>
2015-06-26 16:37:04 -0300

    Implemented ball colors

--- mesa.py
+++ mesa.py
@@ -11,7 +11,7 @@ class Mesa(World):
 
     def __init__(self,
                  gravity=0, friction=0.5, restitution=0.95,
-                 num_balls=1, speed=200, radius=10,
+                 num_balls=15, speed=200, radius=10,
                  color='random'):
 
         super(Mesa, self).__init__(gravity=gravity, dfriction=friction,
@@ -33,32 +33,34 @@ class Mesa(World):
         AABB(0, 800, 0, 100, world=self, mass='inf')     # Baixo
 
         # Inicia bolas
-        # Posição das bolas        
-        bolas = []
+      
+        # Vetor de posição das bolas        
         posX = 200
         posY = 297.5
         pos_bolas = [(posX,posY), (posX-20,posY-10), (posX-20,posY+10), (posX-40, posY-20), 
                     (posX-40, posY), (posX-40, posY+20), (posX-60, posY-30), (posX-60, posY-10),
                     (posX-60,posY+10), (posX-60, posY+30), (posX-80, posY-40), (posX-80, posY-20),
                     (posX-80, posY), (posX-80, posY+20), (posX-80, posY+40)]
-
-        for pos in pos_bolas:
-            vel = (0, 0)
-            bola = Circle(radius=radius, vel=vel, pos=pos, mass=1)
-            bola.color = self.get_color(color)
-            bolas.append(bola)
-            self.add(bola)
     
+        # Vetor de cores das bolas
+        cor_bolas = [(255,255,0), (0,0,255), (255,0,0), (128,0,128), (255,20,147), (0,128,0), (244,164,96), (0,0,0),
+                        (255,215,0), (0,0,205), (139,0,0), (34,139,34), (255,105,180), (138,43,226), (188,143,143)]
+        # Na ordem: amarela, azul, vermelha, violeta, rosa, verde, bege, preta, 
+        #           amarela, azul, vermelha, verde, rosa, violeta, bege
+        
+        #Associa as bolas com as posições e as cores:
+
+        for x in range (num_balls):
+            bola = Circle(radius=radius, mass=1)
+            bola.pos = pos_bolas[x]
+            bola.color = cor_bolas[x]
+            self.add(bola)
+
+        # Define bola branca
         pos = Vec2(uniform(50, 750), uniform(150, 450))
         bolao = Circle(radius=1.5*radius, vel=Vec2(0, 0), pos=pos, mass=2)
         bolao.color = (0, 0, 0)
         #self.add(bolao)
-
-    def get_color(self, color):
-        if color == 'random':
-            return (randint(0, 10)*25, randint(0, 10)*25, randint(0, 10)*25)
-        else:
-            return color
 
     @listen('key-down', 'space')
     def toggle_pause(self):
