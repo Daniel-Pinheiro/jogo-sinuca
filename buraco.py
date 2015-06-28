574bfeecbfa80ff958f1911b18e389e677d85db8
Daniel <sbd_action@hotmail.com>
2015-06-28 13:09:04 -0300

    Adicionado descrição do buraco

--- buraco.py
+++ buraco.py
@@ -3,6 +3,11 @@ from FGAme import *
 
 #  Esta classe serve para implementar melhor os buracos, pois quando são
 #  apenas circulos, eles acabam colidindo com as bolas.
+#
+#  Um buraco é composto de três circulos,com um dentro do outro, onde o 
+#  maior não possui colisão e tem o tamanho do buraco, o médio, ao colidir,
+#  começa a atrair a bola e o menor, ao colidir, tira a bola de jogo e
+#  incrementa um dos placares.
 
 class Buraco(Circle):
 
