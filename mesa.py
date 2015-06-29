c20c8ca6bb996950dfb26067d29e6c7288f06df6
Daniel <sbd_action@hotmail.com>
2015-06-29 02:35:00 -0300

    Algumas modificações na organização

--- mesa.py
+++ mesa.py
@@ -11,7 +11,6 @@ restitution=0.95
 num_balls=15
 speed=200
 radius=10
-color='all'
 
 
 # Inicializa a mesa com as bolas
@@ -25,20 +24,23 @@ class Mesa(World):
         
         # Buracos da mesa
         pos_buracos = [(30, 495), (770, 495), (400, 495), (400, 100),(30, 100), (770, 100)]
+        self.listaBuracos = []
+
+        self.buracos = [Circle(radius=25, vel=Vec2(0, 0), pos=Vec2(pos), mass='inf',color=(30,30,30), col_layer=2,**self)
+                        for pos in pos_buracos]
 
         for pos in pos_buracos:
-            buraco = Circle(radius=25, vel=Vec2(0, 0), pos=Vec2(pos), mass='inf')
-            self.centroBuraco = Circle(radius=10, pos=pos, mass='inf', color='blue', col_layer=1)
-            self.centroBuraco.listen('collision', self.bola_no_buraco)
-            self.add(self.centroBuraco)
-            self.add(buraco)
+            centroBuraco = Circle(radius=10, pos=pos, mass='inf', color='blue')
+            self.listaBuracos.append(centroBuraco)
+            self.add(centroBuraco)
+            centroBuraco.listen('collision', self.bola_no_buraco)
         
         # Limites da mesa (740 x 395)
         #   xi, xf, yi, yf 
-        AABB(0, 800, 495, 800, world=self, mass='inf', col_layer=1)  # Cima
-        AABB(0, 30, 30, 800, world=self, mass='inf', col_layer=1)   # Esquerda
-        AABB(770, 800, 30, 800, world=self, mass='inf', col_layer=1) # Direita
-        AABB(0, 800, 0, 100, world=self, mass='inf', col_layer=1)     # Baixo
+        AABB(0, 800, 495, 800, world=self, mass='inf')  # Cima
+        AABB(0, 30, 30, 800, world=self, mass='inf')   # Esquerda
+        AABB(770, 800, 30, 800, world=self, mass='inf') # Direita
+        AABB(0, 800, 0, 100, world=self, mass='inf')     # Baixo
 
         # Inicia bolas
       
@@ -59,7 +61,7 @@ class Mesa(World):
         #Cria as bolas e as associa com as posições e as cores:
 
         for x in range (num_balls):
-            bola = Circle(radius=radius, mass=1, col_layer=1)
+            bola = Circle(radius=radius, mass=1)
             bola.pos = pos_bolas[x]
             bola.color = cor_bolas[x]
             bola.numero = x
@@ -67,30 +69,45 @@ class Mesa(World):
 
         # Define bola branca
         pos = (600, 100 + 395/2)
-        self.bolao = Circle(radius=1.5*radius, vel=(150, 157), pos=pos, mass=2, col_layer=1)
+        self.bolao = Circle(radius=1.5*radius, vel=(-300, 0), pos=pos, mass=2)
         self.bolao.color = (255,255,255)
         self.add(self.bolao)
 
-
-
-    @listen('collision')
-    def bola_no_buraco(self, col):
-        self.remove(col.objects[1])
-        print(col.objects)
-        
+    @listen("frame-enter")
+    def aplica_colisoes(self):
+        pass
+    
     @listen('key-down', 'return')
     def toggle_pause(self):
         super(Mesa, self).toggle_pause()
 
     # Teste de tacada
-    @listen('key-down', 'space', -200 )
-    def tacada(self, impulso):
+    @listen('key-down', 'up', 200 )
+    def tacadau(self, impulso):
+        direcao = Vec2(0,1)
+        self.bolao.boost(direcao*impulso)
+
+    @listen('key-down', 'down', 200 )
+    def tacadad(self, impulso):
+        direcao = Vec2(0,-1)
+        self.bolao.boost(direcao*impulso)
+
+    @listen('key-down', 'left', 200 )
+    def tacadal(self, impulso):
+        direcao = Vec2(-1,0)
+        self.bolao.boost(direcao*impulso)
+
+    @listen('key-down', 'right', 200 )
+    def tacadar(self, impulso):
         direcao = Vec2(1,0)
         self.bolao.boost(direcao*impulso)
 
-    
+    #@listen('collision')
+    def bola_no_buraco(self, col):
+        self.remove(col.objects[1])
+        print(col.objects)
 
-# Inicia o jogo
+        # Inicia o jogo
 if __name__ == '__main__':
-    w = Mesa()
-    w.run()
+    game = Mesa()
+    game.run()
